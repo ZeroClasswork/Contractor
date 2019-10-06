4342391637e9dab6b9966aa893acd14ba6a6a23c
atleastzero <megan.obryan52@gmail.com>
2019-10-05 17:59:41 -0700

    Set up routes in app.py

--- app.py
+++ app.py
@@ -7,5 +7,35 @@ def index():
     """Return homepage."""
     return render_template("home.html")
 
+@app.route('/secrets')
+def secrets_index():
+    """Show all secrets"""
+    return render_template("secrets_index.html")
+
+@app.route('/secrets/new')
+def secrets_new():
+    """Create a new playlist"""
+    return render_template("secrets_new.html")
+
+@app.route('/secrets', methods=["POST"])
+def secrets_submit():
+    pass
+
+@app.route('/secrets/:id')
+def secrets_show():
+    pass
+
+@app.route('/secrets/:id/edit')
+def secrets_edit():
+    pass
+
+@app.route('/secrets/:id', methods=["POST"])
+def secrets_update():
+    pass
+
+@app.route('/secrets/:id/delete', methods=["POST"])
+def secrets_delete():
+    pass
+
 if __name__ == '__main__':
     app.run(debug=True)
