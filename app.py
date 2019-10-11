3c9983b04bd4e44c19245bb378356491ec797f84
atleastzero <megan.obryan52@gmail.com>
2019-10-10 23:49:09 -0700

    Pushing to heroku

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
 from datetime import datetime
 from bson.objectid import ObjectId
+import os
 
-client = MongoClient()
-db = client.Contractor
+host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Contractor')
+client = MongoClient(host=f'{host}?retryWrites=false')
+db = client.get_default_database()
 secrets = db.secrets
 
 app = Flask(__name__)
@@ -72,4 +74,4 @@ def secrets_delete(secret_id):
     return redirect(url_for('secrets_index'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+  app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
